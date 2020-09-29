bb29ee99d8f211216650de6773559ae071d9dd00
Tim Kranen <tim.kranen@rabobank.nl>
2020-09-29 21:19:36 +0200

    Annotated functions with comments to give some clarity on the fucntion

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,13 +5,10 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
+                degradeItem(i)
             } else {
                 if (items[i].quality < 50) {
+                    // This block increases quality, for backstage passes it increases the quality multiple times based on sellIn value
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
@@ -30,6 +27,7 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
+            // Decrease sellIn value
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                 items[i].sellIn = items[i].sellIn - 1
             }
@@ -37,19 +35,26 @@ class GildedRose(var items: Array<Item>) {
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
+                        // Decrease quality twice as fast, since sellIn value < 0
+                        degradeItem(i)
                     } else {
+                        // Set item quality to 0, because of backstage pass requirement
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
                     if (items[i].quality < 50) {
+                        // Increase quality a second time for aged brie
                         items[i].quality = items[i].quality + 1
                     }
                 }
+            }
+        }
+    }
+
+    private fun degradeItem(i: Int) {
+        if (items[i].quality > 0) {
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].quality = items[i].quality - 1
             }
         }
     }
